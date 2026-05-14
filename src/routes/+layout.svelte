6b8dae1d47443436e5634f2849bc295ad8d76ce8
alexisdev23 <alexisescobardev@gmail.com>
2026-05-13 22:40:19 -0300

    feat: implements components in layout and page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/components/Header.svelte';
+	import Sidebar from '$lib/components/sidebar.svelte';
+	import Bottombar from '$lib/components/bottombar.svelte';
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<ModeWatcher />
+<div class="flex h-screen overflow-hidden">
+	<div class="hidden md:flex">
+		<Sidebar />
+	</div>
+
+	<div class="flex flex-1 flex-col overflow-hidden">
+		<Header />
+		<main class="flex-1 overflow-auto p-6 pb-20 md:pb-6">
+			{@render children()}
+		</main>
+	</div>
+
+	<div class="md:hidden">
+		<Bottombar />
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import ModeToggle from '$lib/components/ui/button/modeToggle.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button>Hola</Button>
+<ModeToggle />
